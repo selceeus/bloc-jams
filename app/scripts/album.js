0eb43e7e63e62bbff4f3508743e2b9b668bd5435
selceeus <matt@matthewa.com>
2015-03-12 03:42:18 +0000

    Added functionality to player controls

--- app/scripts/album.js
+++ app/scripts/album.js
@@ -35,7 +35,7 @@
  var createSongRow = function(songNumber, songName, songLength){
     var template =
        '<tr>'
-+ '  <td class="song-number col-md-1" data-song-number="' + songNumber + '">' + songNumber + '</td>'
+     + '  <td class="song-number col-md-1" data-song-number="' + songNumber + '">' + songNumber + '</td>'
      + '  <td class="col-md-9">' + songName + '</td>'
      + '  <td class="col-md-2">' + songLength + '</td>'
      + '</tr>'
@@ -116,14 +116,53 @@
 
       }
 
- }
+ };
 
+ var updateSeekPercentage = function($seekBar, event) {
+   var barWidth = $seekBar.width();
+   var offsetX = event.pageX - $seekBar.offset().left;
+ 
+   var offsetXPercent = (offsetX  / barWidth) * 100;
+   offsetXPercent = Math.max(0, offsetXPercent);
+   offsetXPercent = Math.min(100, offsetXPercent);
+ 
+   var percentageString = offsetXPercent + '%';
+   $seekBar.find('.fill').width(percentageString);
+   $seekBar.find('.thumb').css({left: percentageString});
+ };
+
+ var setupSeekBars = function() {
+ 
+   $seekBars = $('.player-bar .seek-bar');
+   $seekBars.click(function(event) {
+     updateSeekPercentage($(this), event);
+   });
+ 
+  $seekBars.find('.thumb').mousedown(function(event){
+    
+    var $seekBar = $(this).parent();
+
+    $seekBar.addClass('no-animate');
+ 
+    $(document).bind('mousemove.thumb', function(event){
+      updateSeekPercentage($seekBar, event);
+    });
+ 
+    //cleanup
+    $(document).bind('mouseup.thumb', function(){
+      $seekBar.addClass('no-animate');
+      $(document).unbind('mousemove.thumb');
+      $(document).unbind('mouseup.thumb');
+    });
+ 
+  });
+  };
+  
 if(document.URL.match(/\/album.html/)) {
   
   $(document).ready(function() {
-      
       changeAlbumView(albumPicasso);
-      
+      setupSeekBars();
   });
 
 }
--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -128,7 +128,7 @@ require.register("scripts/album", function(exports, require, module) {
  var createSongRow = function(songNumber, songName, songLength){
     var template =
        '<tr>'
-+ '  <td class="song-number col-md-1" data-song-number="' + songNumber + '">' + songNumber + '</td>'
+     + '  <td class="song-number col-md-1" data-song-number="' + songNumber + '">' + songNumber + '</td>'
      + '  <td class="col-md-9">' + songName + '</td>'
      + '  <td class="col-md-2">' + songLength + '</td>'
      + '</tr>'
@@ -209,14 +209,53 @@ require.register("scripts/album", function(exports, require, module) {
 
       }
 
- }
+ };
 
+ var updateSeekPercentage = function($seekBar, event) {
+   var barWidth = $seekBar.width();
+   var offsetX = event.pageX - $seekBar.offset().left;
+ 
+   var offsetXPercent = (offsetX  / barWidth) * 100;
+   offsetXPercent = Math.max(0, offsetXPercent);
+   offsetXPercent = Math.min(100, offsetXPercent);
+ 
+   var percentageString = offsetXPercent + '%';
+   $seekBar.find('.fill').width(percentageString);
+   $seekBar.find('.thumb').css({left: percentageString});
+ };
+
+ var setupSeekBars = function() {
+ 
+   $seekBars = $('.player-bar .seek-bar');
+   $seekBars.click(function(event) {
+     updateSeekPercentage($(this), event);
+   });
+ 
+  $seekBars.find('.thumb').mousedown(function(event){
+    
+    var $seekBar = $(this).parent();
+
+    $seekBar.addClass('no-animate');
+ 
+    $(document).bind('mousemove.thumb', function(event){
+      updateSeekPercentage($seekBar, event);
+    });
+ 
+    //cleanup
+    $(document).bind('mouseup.thumb', function(){
+      $seekBar.addClass('no-animate');
+      $(document).unbind('mousemove.thumb');
+      $(document).unbind('mouseup.thumb');
+    });
+ 
+  });
+  };
+  
 if(document.URL.match(/\/album.html/)) {
   
   $(document).ready(function() {
-      
       changeAlbumView(albumPicasso);
-      
+      setupSeekBars();
   });
 
 }
