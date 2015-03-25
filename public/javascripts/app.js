f276a4a8f12dc6c9c2025a85c06b9e497f1e758f
selceeus <matt@matthewa.com>
2015-03-25 03:32:53 +0000

    Converted Bloc Jams to a single page application

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -267,8 +267,20 @@ if(document.URL.match(/\/album.html/)) {
 //require("./album");
 //require("./profile");
 
-angular.module('BlocJams',[]).controller('Landing.controller', ['$scope',function($scope) {
-  console.log("Landing Controller");
+blocJams = angular.module('BlocJams', ['ui.router']);
+
+blocJams.config(['$stateProvider', '$locationProvider', function($stateProvider, $locationProvider) {
+  $locationProvider.html5Mode(true);
+
+  $stateProvider.state('landing', {
+    url: '/',
+    controller: 'Landing.controller',
+    templateUrl: '/templates/landing.html'
+  });
+
+}]);
+
+blocJams.controller('Landing.controller', ['$scope', function($scope) {
   $scope.subText = "Turn the music up!";
 
   $scope.subTextClicked = function() {
