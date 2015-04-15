6dafc00babc7357a40449f9c0d2c4480a8c458bb
selceeus <matt@matthewa.com>
2015-04-15 02:06:04 +0000

    Added a time indicator for song progress

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -112,9 +112,16 @@ blocJams.controller('Album.controller', ['$scope', 'SongPlayer', function($scope
 
 blocJams.controller('PlayerBar.controller', ['$scope', 'SongPlayer', function($scope, SongPlayer) {
   $scope.songPlayer = SongPlayer;
+
+  SongPlayer.onTimeUpdate(function(event, time){
+     $scope.$apply(function(){
+       $scope.playTime = time;
+     });
+   });
+
 }]);
 
-blocJams.service('SongPlayer', function() {
+blocJams.service('SongPlayer', ['$rootScope', function($rootScope) {
   var currentSoundFile = null;
   var trackIndex = function(album, song) {
     return album.songs.indexOf(song);
@@ -159,22 +166,32 @@ blocJams.service('SongPlayer', function() {
          currentSoundFile.setTime(time);
        }
      },
+    onTimeUpdate: function(callback) {
+      return $rootScope.$on('sound:timeupdate', callback);
+    },
     setSong: function(album, song) {
+      
       if (currentSoundFile) {
         currentSoundFile.stop();
       }
+
       this.currentAlbum = album;
       this.currentSong = song;
-      
+  
       currentSoundFile = new buzz.sound(song.audioUrl, {
         formats: [ "mp3" ],
         preload: true
       });
- 
+
+      currentSoundFile.bind('timeupdate', function(e){
+        $rootScope.$broadcast('sound:timeupdate', this.getTime());
+      });
+  
       this.play();
     }
-  };
-  });
+  }
+ }]);
+
   blocJams.directive('slider', ['$document', function($document){
      // Returns a number between 0 and 1 to determine where the mouse event happened along the slider bar.
      var calculateSliderPercentFromMouseEvent = function($slider, event) {
@@ -267,4 +284,32 @@ blocJams.service('SongPlayer', function() {
       }
     }
   }]);
+blocJams.filter('timecode', function(){
+   return function(seconds) {
+     seconds = Number.parseFloat(seconds);
+ 
+     // Returned when no time is provided.
+     if (Number.isNaN(seconds)) {
+       return '-:--';
+     }
+ 
+     // make it a whole number
+     var wholeSeconds = Math.floor(seconds);
+ 
+     var minutes = Math.floor(wholeSeconds / 60);
+ 
+     remainingSeconds = wholeSeconds % 60;
+ 
+     var output = minutes + ':';
+ 
+     // zero pad seconds, so 9 seconds should be :09
+     if (remainingSeconds < 10) {
+       output += '0';
+     }
+ 
+     output += remainingSeconds;
+ 
+     return output;
+   }
+ })
 
